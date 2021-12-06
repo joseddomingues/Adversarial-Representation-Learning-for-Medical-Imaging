022c768e28b8d973e7f97263bccb0f9fbadae7ab
José Domingues <jdavidomingues@gmail.com>
2021-12-06 19:19:59 +0000

    Argument Parser for API segmentation

--- MedSegmentation/api.py
+++ MedSegmentation/api.py
@@ -1,53 +1,80 @@
 import os
-from sys import argv
+from argparse import ArgumentParser
 
 import cv2
 import torch
 from torch.autograd import Variable
 
-checkpoints_directory_unet = "checkpoints_unet"
+# Creates argument parser
+arg = ArgumentParser()
 
-script, img_path = argv
-# python api.py /path/to/image/imagename.extension 
-# will give output in the folder containing script as Output_unet.png
+arg.add_argument('--model_dir', required=True, help='Checkpoints Model Directory Path')
+arg.add_argument('--test_images', required=True, help='Test Images Directory Path')
+arg.add_argument('--output_folder', required=True, help='Output Folder To Save Segmentation Masks')
 
+opt_map = arg.parse_args()
+
+# Loads the model from the checkpoints
+checkpoints_directory_unet = opt_map.model_dir  # "checkpoints_unet"
 checkpoints_unet = os.listdir(checkpoints_directory_unet)
 checkpoints_unet.sort(key=lambda x: int((x.split('_')[2]).split('.')[0]))
 model_unet = torch.load(checkpoints_directory_unet + '/' + checkpoints_unet[-1])
 
-model_unet.eval()
-if torch.cuda.is_available():  # use gpu if available
-    model_unet.cuda()
-
-image = cv2.imread(img_path)
-orig_width, orig_height = image.shape[0], image.shape[1]
-input_unet = image
-
-input_unet = cv2.resize(input_unet, (256, 256), interpolation=cv2.INTER_CUBIC)
-input_unet = input_unet.reshape((256, 256, 3, 1))
-
-input_unet = input_unet.transpose((3, 2, 0, 1))
-
-input_unet.astype(float)
-input_unet = input_unet / 255
-
-input_unet = torch.from_numpy(input_unet)
-
-input_unet = input_unet.type(torch.FloatTensor)
-
-if torch.cuda.is_available():  # use gpu if available
-    input_unet = Variable(input_unet.cuda())
+# Check and create if needed output folder
+if not os.path.exists(opt_map.output_folder):
+    os.mkdir(opt_map.output_folder)
 else:
-    input_unet = Variable(input_unet)
+    os.remove(opt_map.output_folder)
+    os.mkdir(opt_map.output_folder)
 
-out_unet = model_unet(input_unet)
 
-out_unet = out_unet.cpu().data.numpy()
+def _segment_image(image, model):
+    """
+    Apply segmentation on a given image with a given model
+    @param image: Image to segment
+    @param model: Model to use for segmentation
+    @return: The segmentation for that image
+    """
+    model.eval()
+    if torch.cuda.is_available():  # use gpu if available
+        model.cuda()
 
-out_unet = out_unet * 255
+    image = cv2.imread(image)
+    orig_width, orig_height = image.shape[0], image.shape[1]
+    input_unet = image
 
-out_unet = out_unet.transpose((2, 3, 0, 1))
-out_unet = out_unet.reshape((256, 256, 1))
-out_unet = cv2.resize(out_unet, (orig_height, orig_width), interpolation=cv2.INTER_CUBIC)
+    input_unet = cv2.resize(input_unet, (256, 256), interpolation=cv2.INTER_CUBIC)
+    input_unet = input_unet.reshape((256, 256, 3, 1))
 
-cv2.imwrite("Output_unet.png", out_unet)
+    input_unet = input_unet.transpose((3, 2, 0, 1))
+
+    input_unet.astype(float)
+    input_unet = input_unet / 255
+
+    input_unet = torch.from_numpy(input_unet)
+
+    input_unet = input_unet.type(torch.FloatTensor)
+
+    if torch.cuda.is_available():  # use gpu if available
+        input_unet = Variable(input_unet.cuda())
+    else:
+        input_unet = Variable(input_unet)
+
+    out_unet = model(input_unet)
+
+    out_unet = out_unet.cpu().data.numpy()
+
+    out_unet = out_unet * 255
+
+    out_unet = out_unet.transpose((2, 3, 0, 1))
+    out_unet = out_unet.reshape((256, 256, 1))
+    out_unet = cv2.resize(out_unet, (orig_height, orig_width), interpolation=cv2.INTER_CUBIC)
+    return out_unet
+
+
+# For each image, segment the image and save it to the output folder
+for image in os.listdir(opt_map.test_images):
+    curr_image = opt_map.output_folder + '/' + image
+    result = _segment_image(curr_image, model_unet)
+    curr_image = curr_image.replace('.png', '_mask.png')
+    cv2.imwrite(curr_image, result)
