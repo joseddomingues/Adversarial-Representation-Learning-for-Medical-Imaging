Fixed some bugs in the half precision implementation

MedSinGAN/functions.py
@@ -163,7 +163,7 @@ def sample_random_noise(depth, reals_shapes, opt):
     return noise
 
 
-def calc_gradient_penalty(netD, real_data, fake_data, LAMBDA, device, scaler):
+def calc_gradient_penalty(netD, real_data, fake_data, LAMBDA, device):
     """
 
     @param netD:
@@ -184,8 +184,7 @@ def calc_gradient_penalty(netD, real_data, fake_data, LAMBDA, device, scaler):
         interpolates = [i.to(device) for i in interpolates]
         interpolates = [torch.autograd.Variable(i, requires_grad=True) for i in interpolates]
 
-        with autocast():
-            disc_interpolates = netD(interpolates)
+        disc_interpolates = netD(interpolates)
 
     else:
         alpha = torch.rand(1, 1)
@@ -196,27 +195,19 @@ def calc_gradient_penalty(netD, real_data, fake_data, LAMBDA, device, scaler):
         interpolates = interpolates.to(device)  # .cuda()
         interpolates = torch.autograd.Variable(interpolates, requires_grad=True)
 
-        with autocast():
-            disc_interpolates = netD(interpolates)
+        disc_interpolates = netD(interpolates)
 
-    disc_interpolates = scaler.scale(disc_interpolates)
     gradients = torch.autograd.grad(outputs=disc_interpolates, inputs=interpolates,
                                     grad_outputs=torch.ones(disc_interpolates.size()).to(device),
                                     # .cuda(), #if use_cuda else torch.ones(
                                     # disc_interpolates.size()),
                                     create_graph=True, retain_graph=True, only_inputs=True)[0]
 
-    # Prepares gradients unscaled for grandient penalty calculation
-    inv_scale = 1. / scaler.get_scale()
-    grad_params = [p * inv_scale for p in gradients]
-
     # LAMBDA = 1
-    with autocast():
-        gradient_penalty = ((grad_params.norm(2, dim=1) - 1) ** 2).mean() * LAMBDA
+    gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean() * LAMBDA
 
     del interpolates
     del gradients
-    del grad_params
 
     return gradient_penalty
 

MedSinGAN/training.py
@@ -121,7 +121,8 @@ def train_single_scale(netD, netG, reals, fixed_noise, noise_amp, opt, depth, wr
     """
 
     # Creates scaler for half precision
-    scaler = GradScaler()
+    d_scaler = GradScaler()
+    g_scaler = GradScaler()
 
     # Get the shapes of the different scales and then the current real image (According to current scale)
     reals_shapes = [real.shape for real in reals]
@@ -204,7 +205,7 @@ def train_single_scale(netD, netG, reals, fixed_noise, noise_amp, opt, depth, wr
             RMSE = torch.sqrt(rec_loss).detach()
             _noise_amp = opt.noise_amp_init * RMSE
 
-        noise_amp[-1] = scaler.scale(_noise_amp)
+        noise_amp[-1] = g_scaler.scale(_noise_amp)
 
         del z_reconstruction
 
@@ -243,13 +244,13 @@ def train_single_scale(netD, netG, reals, fixed_noise, noise_amp, opt, depth, wr
                 errD_fake = output.mean()
 
             # calculate penalty, do backward pass and step
-            gradient_penalty = functions.calc_gradient_penalty(netD, real, fake, opt.lambda_grad, opt.device, scaler)
+            gradient_penalty = functions.calc_gradient_penalty(netD, real, fake, opt.lambda_grad, opt.device, d_scaler)
 
             with autocast():
                 errD_total = errD_real + errD_fake + gradient_penalty
 
-            scaler.scale(errD_total).backward()
-            scaler.step(optimizerD)
+            d_scaler.scale(errD_total).backward()
+            d_scaler.step(optimizerD)
 
         del noise
 
@@ -275,11 +276,11 @@ def train_single_scale(netD, netG, reals, fixed_noise, noise_amp, opt, depth, wr
         with autocast():
             errG_total = errG + rec_loss
 
-        scaler.scale(errG_total).backward()
+        g_scaler.scale(errG_total).backward()
 
         # optimizer applied G number of steps
         for _ in range(opt.Gsteps):
-            scaler.step(optimizerG)
+            g_scaler.step(optimizerG)
 
         ############################
         # (3) Log Metrics
@@ -307,7 +308,8 @@ def train_single_scale(netD, netG, reals, fixed_noise, noise_amp, opt, depth, wr
             functions.save_image('{}/reconstruction_{}.jpg'.format(opt.outf, iter + 1), rec.detach())
             generate_samples(netG, opt, depth, noise_amp, writer, reals, iter + 1)
 
-        scaler.update()
+        d_scaler.update()
+        g_scaler.update()
         schedulerD.step()
         schedulerG.step()
 
@@ -321,7 +323,7 @@ def train_single_scale(netD, netG, reals, fixed_noise, noise_amp, opt, depth, wr
         # break
 
     # saves the networks
-    functions.save_networks(netG, netD, z_opt, opt, scaler)
+    functions.save_networks(netG, netD, z_opt, opt, d_scaler)
     return fixed_noise, noise_amp, netG, netD
 
 

MedSinGAN/training_harmonization_editing.py
@@ -135,7 +135,8 @@ def train_single_scale(netD, netG, reals, img_to_augment, naive_img, naive_img_l
     """
 
     # Creates scaler for half precision
-    scaler = GradScaler()
+    d_scaler = GradScaler()
+    g_scaler = GradScaler()
 
     # Get the shapes of the different scales and then the current real image (According to current scale)
     reals_shapes = [real.shape for real in reals]
@@ -217,7 +218,7 @@ def train_single_scale(netD, netG, reals, img_to_augment, naive_img, naive_img_l
                 RMSE = torch.sqrt(rec_loss).detach()
                 _noise_amp = opt.noise_amp_init * RMSE
 
-            noise_amp[-1] = scaler.scale(_noise_amp)
+            noise_amp[-1] = g_scaler.scale(_noise_amp)
 
             del z_reconstruction
 
@@ -279,15 +280,13 @@ def train_single_scale(netD, netG, reals, img_to_augment, naive_img, naive_img_l
                 output = netD(fake.detach().clone())
                 errD_fake = output.mean()
 
-            # calculate penalty, do backward pass and step
-            gradient_penalty = functions.calc_gradient_penalty(netD, real, fake, opt.lambda_grad, opt.device, scaler)
-
-            with autocast():
+                # calculate penalty, do backward pass and step
+                gradient_penalty = functions.calc_gradient_penalty(netD, real, fake, opt.lambda_grad, opt.device)
                 errD_total = errD_real + errD_fake + gradient_penalty
 
-            scaler.scale(errD_total).backward()
-            scaler.step(optimizerD)
+            d_scaler.scale(errD_total).backward()
 
+        d_scaler.step(optimizerD)
         del noise
 
         ############################
@@ -311,10 +310,10 @@ def train_single_scale(netD, netG, reals, img_to_augment, naive_img, naive_img_l
         with autocast():
             errG_total = errG + rec_loss
 
-        scaler.scale(errG_total).backward()
+        g_scaler.scale(errG_total).backward()
 
-        for _ in range(opt.Gsteps):
-            scaler.step(optimizerG)
+        # for _ in range(opt.Gsteps):
+        g_scaler.step(optimizerG)
 
         ############################
         # (3) Log Metrics
@@ -322,10 +321,10 @@ def train_single_scale(netD, netG, reals, img_to_augment, naive_img, naive_img_l
         log_metric('Discriminator Train Loss Real', -errD_real.item(), step=iter + 1)
         log_metric('Discriminator Train Loss Fake', errD_fake.item(), step=iter + 1)
         log_metric('Discriminator Train Loss Gradient Penalty', gradient_penalty.item(), step=iter + 1)
-        log_metric('Discriminator Loss', errD_total, step=iter + 1)
+        log_metric('Discriminator Loss', errD_total.item(), step=iter + 1)
         log_metric('Generator Train Loss', errG.item(), step=iter + 1)
         log_metric('Generator Train Loss Reconstruction', rec_loss.item(), step=iter + 1)
-        log_metric('Generator Loss', errG_total, step=iter + 1)
+        log_metric('Generator Loss', errG_total.item(), step=iter + 1)
 
         ############################
         # (4) Log Results
@@ -346,12 +345,13 @@ def train_single_scale(netD, netG, reals, img_to_augment, naive_img, naive_img_l
         #     generate_samples(netG, img_to_augment, naive_img, naive_img_large, aug, opt, depth,
         #                      noise_amp, writer, reals, iter + 1)
 
-        scaler.update()
+        d_scaler.update()
+        g_scaler.update()
         schedulerD.step()
         schedulerG.step()
 
     # saves the networks
-    functions.save_networks(netG, netD, z_opt, opt, scaler)
+    functions.save_networks(netG, netD, z_opt, opt, d_scaler)
     return fixed_noise, noise_amp, netG, netD
 
 