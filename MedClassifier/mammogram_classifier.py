968294ee5e217194e42f68f0985acb28e4d46add
José Domingues <jdavidomingues@gmail.com>
2022-04-01 17:40:41 +0100

    Resnet version to resnet18

--- MedClassifier/mammogram_classifier.py
+++ MedClassifier/mammogram_classifier.py
@@ -7,7 +7,7 @@ class MammogramClassifier(nn.Module):
         super(MammogramClassifier, self).__init__()
 
         self.n_classes = n_classes
-        self.model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet152', pretrained=False)
+        self.model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', pretrained=False)
         self.fc_layer = nn.Linear(1000, self.n_classes)
 
     def forward(self, x):
