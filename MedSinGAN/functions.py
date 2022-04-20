b2b20d09a05867f6d1a5c98a824c956a29194460
José Domingues <jdavidomingues@gmail.com>
2022-04-20 11:20:25 +0100

    Removed verbose

--- MedSinGAN/functions.py
+++ MedSinGAN/functions.py
@@ -705,7 +705,8 @@ class EarlyStopper:
             self.save_checkpoint(val_loss, netG, netD, z_opt, opt, g_scaler)
         elif score < self.best_score + self.delta:
             self.counter += 1
-            self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.verbose:
+                self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
             if self.counter >= self.patience:
                 self.early_stop = True
         else:
