0d3d8574f1eeb397c8fb6ec938b83f2480133551
José Domingues <jdavidomingues@gmail.com>
2022-09-08 19:40:54 +0100

    Harmoniser samples generator added to complete writing

--- utils/HarmoniserInputSamples.py
+++ utils/HarmoniserInputSamples.py
@@ -1,3 +1,4 @@
+import math
 import sys
 
 sys.path.append("../")
@@ -6,20 +7,17 @@ import MedSinGAN.functions as functions
 from MedSinGAN.config import get_arguments
 
 if __name__ == "__main__":
-    # TODO: Adjust these values to run on a colab notebook
+
     parser = get_arguments()
     parser.add_argument('--input_name', type=str, default="")
-    parser.add_argument('--nc_im', type=int, default=0)
-    parser.add_argument('--not_cuda', type=bool, default=True)
+    parser.add_argument('--nc_im', type=int, default=3)
+    parser.add_argument('--not_cuda', type=bool, default=False)
 
-    parser.add_argument('--im_max_size', type=int, default=0)
-    parser.add_argument('--scale1', type=int, default=0)
-    parser.add_argument('--stop_scale', type=int, default=0)
-    parser.add_argument('--train_stages', type=int, default=0)
-    parser.add_argument('--scale_factor', type=str, default="")
-    parser.add_argument('--im_min_size', type=int, default=0)
+    parser.add_argument('--im_max_size', type=int, default=614)
+    parser.add_argument('--train_stages', type=int, default=24)
+    parser.add_argument('--im_min_size', type=int, default=120)
 
-    parser.add_argument('--train_mode', type=str, default="")
+    parser.add_argument('--train_mode', type=str, default="harmonization")
 
     opt = parser.parse_args()
 
@@ -30,6 +28,9 @@ if __name__ == "__main__":
     # Adjusts the scales of the image
     # Needs: im_max_size, scale1, stop_scale, train_stages, scale_factor, im_min_size,
     real = functions.adjust_scales2image(real, opt)
+    opt.scale1 = min(opt.im_max_size / max([real.shape[2], real.shape[3]]), 1)
+    opt.stop_scale = opt.train_stages - 1
+    opt.scale_factor = math.pow(opt.im_min_size / (min(real.shape[2], real.shape[3])), 1 / opt.stop_scale)
 
     # Create the scales reals pyramids
     # Needs: train_mode, stop_scale, scale_factor,
